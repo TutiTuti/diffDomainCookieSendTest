017326afef33551687c414198a0fd0b444e1a46f
최용수 <37789623+TutiTuti@users.noreply.github.com>
2025-04-10 23:43:54 +0900

    Update CookieController.java

--- demo/src/main/java/com/example/demo/controller/CookieController.java
+++ demo/src/main/java/com/example/demo/controller/CookieController.java
@@ -16,13 +16,8 @@ public class CookieController {
 
     @GetMapping("/api/test-cookie")
     public void testCookie(HttpServletResponse res){
-        Cookie cookie = new Cookie("cookie-test", "123456");
-        cookie.setMaxAge(3600); // 1시간
-        cookie.setPath("/"); // 모든 경로에 대해 쿠키 적용
-        cookie.setSecure(true); // Secure 쿠키 (HTTPS에서만 전달)
-        cookie.setHttpOnly(false); // JS에서 접근하려면 false (XSS 주의)
-//        cookie.setDomain("front.local"); // 클라이언트 도메인에 맞게 설정
-        res.addCookie(cookie);
+        String cookieValue = "cookie-test=123456; Max-Age=3600; Path=/; Secure; SameSite=None; HttpOnly";
+        res.addHeader("Set-Cookie", cookieValue);
         return ;
     }
 //    @GetMapping("/api/test-cookie")
